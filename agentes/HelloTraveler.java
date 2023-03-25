33ef9b6bbb5612f97275f0beba150f72bcbf26ad
Mario-Recio <m.recio.2020@alumnos.urjc.es>
2023-03-25 19:46:36 +0100

    initial commit

---
+++ agentes/HelloTraveler.java
@@ -0,0 +1,54 @@
+package raf.agentes;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import javax.swing.*;
+
+
+/**
+ * The travel agent just opens a dialog, travels to another base and prints
+ * Hello World to the screen. After that it destroys itself.
+ */
+public class HelloTraveler{
+    /**
+     * Just initialize the super class.
+     *
+     * @param name The name of the agent. This name has to be
+     * unique. Normally the KaaribogaBase class provides some
+     * method to generate a unique name.
+     */
+    public HelloTraveler(String name){
+        //super("Traveler_" + name);
+    }
+
+    /**
+     * This is called just after the kaariboga has been created and
+     * the base has set the important fields.
+     */
+    public void onCreate(){
+        InetAddress target = null;
+
+        try{
+            JOptionPane dialog = new JOptionPane();
+            String server = dialog.showInputDialog (null, "Where do you want me to go today?");
+            target = InetAddress.getByName(server);
+        }
+        catch (UnknownHostException e){
+            System.err.println("Error: Could not determine host address!");
+            //fireDestroyRequest();
+        }
+        //destination = new KaaribogaAddress(target, 10101, null);
+        //fireDispatchRequest();
+    }
+
+    /**
+     * This is automically called if the agent arrives on
+     * a base.
+     */
+    public void onArrival(){
+        System.out.println("Hi! I am the traveler. ");
+        //fireDestroyRequest();
+    }
+
+
+}
